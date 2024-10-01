17ad183203fe52e6c3dec68d1fe45a09f47c6f60
+ <+>
2024-10-02 00:18:16 +0300

    Looping lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+stringNames = ['becca', 'monchari', 'atuti']
+eventName = 'wedding'
+function writeCards(stringNames, eventName) {
+    const message = []
+    for (let i=0; i<stringNames.length; i++) {
+        message.push(`Thank you, ${stringNames[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return message
+}
+
+function countDown(int) {
+    int = 10
+    while (int >= 0) {
+        console.log(int --)
+    }
+    
+}
+
